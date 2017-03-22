4678641bb235064df2dc0b15cab584635d7eedd5
p-herbert <pete.a.herbert@gmail.com>
2017-03-22 00:40:17 -0700

    (refactor) Remove awsdb alias

--- alias.zsh
+++ alias.zsh
@@ -1,9 +1,6 @@
 # Use Homebrew vim
 alias vim='/usr/local/bin/vim'
 
-# psql
-alias awsdb='psql --host=truncated-avocados-db.ccmennnvhu6h.us-west-2.rds.amazonaws.com --port=5432 --username=truncados --password --dbname=blog_graph'
-
 # Process Status
 myFunc() {ps aux | grep $1 | grep -v grep;}
 alias pg=myFunc
