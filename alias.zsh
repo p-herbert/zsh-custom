c890a662bfb716012ffefc392fcbba0175806fe2
p-herbert <pete.a.herbert@gmail.com>
2017-03-27 23:56:24 -0700

    (feat) Add highlighting

--- alias.zsh
+++ alias.zsh
@@ -6,3 +6,12 @@ myFunc() {ps aux | grep $1 | grep -v grep;}
 alias pg=myFunc
 
 alias so='source ~/.zshrc'
+
+# Highlight file
+pyg() {
+    pygmentize -f terminal256 -O style=native -g $1 | perl -e 'print ++$i." $_" for <>'
+}
+alias dog=pyg
+
+hilight() {highlight -O ansi $1 | perl -e 'print ++$i." $_" for <>'}
+alias hl=hilight
