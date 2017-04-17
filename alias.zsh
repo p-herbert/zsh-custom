173331caae2c765fc55aff0b4efdabe323bacb16
p-herbert <pete@sunsama.com>
2017-04-17 14:01:51 -0700

    (feat) Sunsama app start scripts

--- alias.zsh
+++ alias.zsh
@@ -15,3 +15,18 @@ alias dog=pyg
 
 hilight() {highlight -O ansi $1 | awk '{printf "%d\t%s\n", NR, $0}'}
 alias hl=hilight
+
+# Start various Sunsama servers easily.
+function sunsama-app {
+    cd ~/sunsama/meteor/sunsama-app; meteor npm install; export ENV=dev; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; meteor; export TZ=UTC;
+}
+function sunsama-notifs {
+    cd ~/sunsama/meteor/sunsama-notifications; meteor npm install; export ENV=dev; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; meteor --port 3002; export TZ=UTC;
+}
+function sunsama-worker {
+    cd ~/sunsama/meteor/sunsama-worker; meteor npm install; export ENV=dev; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; meteor --port 3003; export TZ=UTC;
+}
+function sunsama-realcandy {
+    export ENV=DEV; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; export PORT=8888; export TZ=UTC;
+    cd ~/sunsama; . venv-realcandy/bin/activate; cd realcandy; python -m web.webapp;
+}
