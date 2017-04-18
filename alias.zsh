26d8b94e36dc0897f503e112757195fd12083fee
p-herbert <pete@sunsama.com>
2017-04-18 03:40:31 +0000

    (refactor) Add DEV_PATH

--- alias.zsh
+++ alias.zsh
@@ -17,16 +17,41 @@ hilight() {highlight -O ansi $1 | awk '{printf "%d\t%s\n", NR, $0}'}
 alias hl=hilight
 
 # Start various Sunsama servers easily.
+export DEV_APP=~/Documents/Development/sunsama;
+
 function sunsama-app {
-    cd ~/sunsama/meteor/sunsama-app; meteor npm install; export ENV=dev; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; meteor; export TZ=UTC;
+    cd $DEV_APP/meteor/sunsama-app;
+    meteor npm install;
+    export ENV=dev;
+    export TZ=UTC;
+    export MONGO_URL=mongodb://localhost:3001/meteor;
+    export ALT_MONGO_URL=mongodb://localhost:3001/alt_data;
+    meteor;
 }
 function sunsama-notifs {
-    cd ~/sunsama/meteor/sunsama-notifications; meteor npm install; export ENV=dev; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; meteor --port 3002; export TZ=UTC;
+    cd $DEV_APP/meteor/sunsama-notifications;
+    meteor npm install;
+    export ENV=dev;
+    export TZ=UTC;
+    export MONGO_URL=mongodb://localhost:3001/meteor;
+    export ALT_MONGO_URL=mongodb://localhost:3001/alt_data;
+    meteor --port 3002;
 }
 function sunsama-worker {
-    cd ~/sunsama/meteor/sunsama-worker; meteor npm install; export ENV=dev; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; meteor --port 3003; export TZ=UTC;
+    cd $DEV_APP/meteor/sunsama-worker;
+    meteor npm install;
+    export ENV=dev;
+    export TZ=UTC;
+    export MONGO_URL=mongodb://localhost:3001/meteor;
+    export ALT_MONGO_URL=mongodb://localhost:3001/alt_data;
+    meteor --port 3003;
 }
 function sunsama-realcandy {
-    export ENV=DEV; export MONGO_URL=mongodb://localhost:27017/meteor; export ALT_MONGO_URL=mongodb://localhost:27017/alt_data; export PORT=8888; export TZ=UTC;
-    cd ~/sunsama; . venv-realcandy/bin/activate; cd realcandy; python -m web.webapp;
+    export ENV=DEV;
+    export MONGO_URL=mongodb://localhost:3001/meteor;
+    export ALT_MONGO_URL=mongodb://localhost:3001/alt_data;
+    export PORT=8888;
+    export TZ=UTC;
+    cd $DEV_APP; . venv-realcandy/bin/activate;
+    cd realcandy; python -m web.webapp;
 }
