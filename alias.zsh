e7a3521e2871fbca68fc285a554646288bb8970b
p-herbert <pete@sunsama.com>
2017-06-20 18:06:25 +0000

    (feat) Add function lsn

--- alias.zsh
+++ alias.zsh
@@ -7,6 +7,10 @@ alias pg=myFunc
 
 alias so='source ~/.zshrc'
 
+lsn() { ls -la | awk '{if ($0~/[rwx]/) {"stat -f %A " $9 | getline k; print k,$0;} else {print;}}'; }
+
+#lsn () {ls -l | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
+             #*2^(8-i));if(k)printf("%0o ",k);print}';}
 # Highlight file
 pyg() {
     pygmentize -f terminal256 -O style=native -g $1 | awk '{printf "%d\t%s\n", NR, $0}'
