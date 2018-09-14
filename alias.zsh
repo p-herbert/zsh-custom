4e1d3df26914a965d5043b20ea3f1b1de8c6468a
p-herbert <11701411+p-herbert@users.noreply.github.com>
2018-09-13 23:22:14 -0700

    (feat) Add alias di

--- alias.zsh
+++ alias.zsh
@@ -9,6 +9,7 @@ lsn() { ls -la | awk '{if ($0~/[rwx]/) {fh="stat -f %A " $9; fh | getline k; clo
 alias link='ln -s'
 alias total='du -sh'
 alias size='du -h'
+alias di='diff -arq'
 
 # Highlight file
 pyg() {
