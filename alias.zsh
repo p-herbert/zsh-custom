fa5e736ec2ff4855ddbe50527849d25c5029b24e
p-herbert <11701411+p-herbert@users.noreply.github.com>
2018-09-19 23:19:21 -0700

    (refactor) Group similar functions

--- alias.zsh
+++ alias.zsh
@@ -5,14 +5,9 @@ alias total='du -sh'
 alias size='du -h'
 alias di='diff -arq'
 
-# Process Status
+# Process status
 pg() {
     ps aux | grep $1 | grep -v grep
-}
-
-# Highlight file
-dog() {
-    pygmentize -f terminal256 -O style=native -g $1 | nl -v 0
 }
 
 # Kill process
@@ -20,6 +15,12 @@ gkill() {
     pg $1 | awk '{print $2}' | xargs kill -9
 }
 
+# Highlight file
+dog() {
+    pygmentize -f terminal256 -O style=native -g $1 | nl -v 0
+}
+
+# Highlight file
 hl() {
     highlight -O ansi $1 | nl -v 0
 }
