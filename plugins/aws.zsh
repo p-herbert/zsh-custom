4f3a7ef8732f5c5206edce4ab3e3333b91cfe1b9
p-herbert <11701411+p-herbert@users.noreply.github.com>
2018-10-22 13:55:53 -0700

    (feat) Add alias for 'amplify init'

--- plugins/aws.zsh
+++ plugins/aws.zsh
@@ -2,6 +2,7 @@
 alias amp='amplify'
 alias ampa='amplify add'
 alias ampc='amplify configure'
+alias ampi='amplify init'
 alias amps='amplify status'
 alias ampu='amplify update'
 alias amppb='amplify publish'
