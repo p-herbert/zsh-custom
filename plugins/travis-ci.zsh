65405db80f245cdf6c6ec1b201268aadd28417d7
p-herbert <11701411+p-herbert@users.noreply.github.com>
2019-01-24 16:41:29 -0800

    (refactor) Remove flag --add

--- plugins/travis-ci.zsh
+++ plugins/travis-ci.zsh
@@ -1,6 +1,6 @@
 # Travis-CI #
 #############
 alias tr='travis'
-alias tre='travis encrypt --add'
+alias tre='travis encrypt'
 alias trs='travis show'
-alias tref='travis encrypt-file --add'
+alias tref='travis encrypt-file'
