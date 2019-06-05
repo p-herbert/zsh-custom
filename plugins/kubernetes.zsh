991164875c4bcba651542b879ae32d6317154c34
p-herbert <11701411+p-herbert@users.noreply.github.com>
2019-06-05 00:53:54 -0700

    (refactor) Remap kubernetes aliases

--- plugins/kubernetes.zsh
+++ plugins/kubernetes.zsh
@@ -1,8 +1,8 @@
 # Kubernetes #
 ##############
-alias k='AWS_PROFILE=serverless-admin kubectl'
-alias kd='k delete'
-alias kg='k get -o wide'
-alias kl='k logs -f'
-alias kp='k proxy --port 9888'
-alias kga='k get all -o wide --all-namespaces'
+alias k8='AWS_PROFILE=serverless-admin kubectl'
+alias k8d='k delete'
+alias k8g='k get -o wide'
+alias k8l='k logs -f'
+alias k8p='k proxy --port 9888'
+alias k8ga='k get all -o wide --all-namespaces'
