c74dcb951a05055f11aa7aef4e3862b700475989
p-herbert <11701411+p-herbert@users.noreply.github.com>
2019-06-06 06:20:00 -0700

    (fix) Use correct alias

--- plugins/kubernetes.zsh
+++ plugins/kubernetes.zsh
@@ -1,8 +1,8 @@
 # Kubernetes #
 ##############
 alias k8='AWS_PROFILE=serverless-admin kubectl'
-alias k8d='k delete'
-alias k8g='k get -o wide'
-alias k8l='k logs -f'
-alias k8p='k proxy --port 9888'
-alias k8ga='k get all -o wide --all-namespaces'
+alias k8d='k8 delete'
+alias k8g='k8 get -o wide'
+alias k8l='k8 logs -f'
+alias k8p='k8 proxy --port 9888'
+alias k8ga='k8 get all -o wide --all-namespaces'
