5084d4f20e6951e4197274a07b304e2a86d5eadd
p-herbert <11701411+p-herbert@users.noreply.github.com>
2019-07-31 10:00:25 -0700

    (feat) Add alias for 'kubectl config use-context'

--- plugins/kubernetes.zsh
+++ plugins/kubernetes.zsh
@@ -6,3 +6,5 @@ alias k8g='k8 get -o wide'
 alias k8l='k8 logs -f'
 alias k8p='k8 proxy --port 9888'
 alias k8ga='k8 get all -o wide --all-namespaces'
+# kube switch context
+alias k8s='k8 config use-context'
