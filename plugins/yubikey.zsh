68a8c7363099fc560aa96cac18336e3513720c4e
p-herbert <11701411+p-herbert@users.noreply.github.com>
2023-09-27 13:17:03 -0700

    (feat) Add Yubikey utility functions

--- plugins/yubikey.zsh
+++ plugins/yubikey.zsh
@@ -9,3 +9,27 @@ function mfa() {
 function mfa_token() {
   ykman oath accounts code -s $1
 }
+
+function mfa_console() {
+  token=$(ykman oath accounts code -s $AWS_MFA_ARN)
+  echo "SUCCESS"
+  echo $token | pbcopy
+}
+
+function prod_vpn() {
+  token=$(ykman oath accounts code -s $OPENVPN_PROD_KEY_ID)
+  echo "SUCCESS"
+  echo $token | pbcopy
+}
+
+function dev_vpn() {
+  token=$(ykman oath accounts code -s $OPENVPN_DEV_KEY_ID)
+  echo "SUCCESS"
+  echo $token | pbcopy
+}
+
+function stg_vpn() {
+  token=$(ykman oath accounts code -s $OPENVPN_STG_KEY_ID)
+  echo "SUCCESS"
+  echo $token | pbcopy
+}
